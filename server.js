514e25f2f6c86bf54b19d6174fd2bee5b38c5108
Vincentt-0 <basesiyu@gmail.com>
2025-11-19 23:01:02 +0700

    Praktikum 9, done !

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 8001;
 const userRoutes = require('./routes/user.routes');
+const productRoutes = require('./routes/product.routes');
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
